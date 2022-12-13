c2fb160e6b225488157e74228996d03a1de4e972
Kevin Oleaga <oleaga@outlook.com>
2022-12-12 22:43:52 -0600

    Update sendEmail.js

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,50 +1,12 @@
-estado = "";
-campo = "";
-
-function validarCampos() {
-    if (!document.getElementById("name").value == "") {
-        if (!document.getElementById("email").value == "") {
-            if (!document.getElementById("message").value == "") {
-                estado = "OK";
-            } else {
-                estado = "FAIL";
-                campo = "Mensaje";
-            }
-        } else {
-            estado = "FAIL";
-            campo = "Email";
-        }
-    } else {
-        estado = "FAIL";
-        campo = "Nombre";
-    }
-
-    switch (estado) {
-        case "FAIL":
-            Swal.fire({
-                icon: 'error',
-                title: 'Oops...',
-                text: 'El espacio "' + campo + '" no puede estar vacío'
-            })
-            break;
-    }
-}
-
 function sendEmail() {
     alert("test")
-    validarCampos()
-    alert(estado)
-
-    if (estado == "OK") {
-        alert("aber")
-        Email.send({
-            SecureToken: "2c1fa375-3443-477d-bc0a-0384c5b6aaae",
-            To: document.getElementById('email').value,
-            From: "oleaga717@gmail.com",
-            Subject: "EduNAO",
-            Body: document.getElementById('message').value
-        }.then(
-            window.location.href = "index.html",
-        );
-    }
+    Email.send({
+        SecureToken: "2c1fa375-3443-477d-bc0a-0384c5b6aaae",
+        To: document.getElementById('email').value,
+        From: "oleaga717@gmail.com",
+        Subject: "EduNAO",
+        Body: document.getElementById('message').value
+    }).then(
+        window.location.href = "index.html",
+    );
 }
