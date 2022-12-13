47010167a159d98b7ebc9428a63bca14899c94c9
Kevin Oleaga <oleaga@outlook.com>
2022-12-13 07:56:07 -0600

    Update sendEmail.js

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,4 +1,37 @@
-function sendEmail() {
+function validateEmail() {
+    var emailID = document.getElementById('email').value;
+    atpos = emailID.indexOf("@");
+    dotpos = emailID.lastIndexOf(".");
+    
+    if (atpos < 1 || ( dotpos - atpos < 2 )) {
+       alert("Please enter correct email ID")
+       document.myForm.EMail.focus() ;
+       return false;
+    }
+    return(true);
+ }
+
+ function validate() {
+    if( document.getElementById('name').value == "" ) {
+       alert( "Please provide your name!" );
+       document.myForm.Name.focus() ;
+       return false;
+    }
+    if( document.getElementById('email').value == "" ) {
+        alert( "Please provide your Email!" );
+        document.myForm.EMail.focus() ;
+        return false;
+     }
+     if( document.getElementById('message').value == "" ) {
+        alert( "Please provide your message" );
+        document.myForm.EMail.focus() ;
+        return false;
+     }
+    return( true );
+ }
+
+ function sendEmail() {
+    alert("test")
     Email.send({
         SecureToken: "2c1fa375-3443-477d-bc0a-0384c5b6aaae",
         To: document.getElementById('email').value,
@@ -6,6 +39,6 @@ function sendEmail() {
         Subject: "EduNAO",
         Body: document.getElementById('message').value
     }).then(
-        window.location.href = "index.html"
+        window.location.href = "index.html",
     );
 }
