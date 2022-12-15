4a153b5f032380ab270049dee995c944a74617d5
Kevin Oleaga <oleaga@outlook.com>
2022-12-15 15:09:30 -0600

    Update sendEmail.js

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,12 +1,11 @@
- function sendEmail() {
-    alert("test")
-    Email.send({
-        SecureToken: "2c1fa375-3443-477d-bc0a-0384c5b6aaae",
-        To: document.getElementById('email').value,
-        From: "oleaga717@gmail.com",
-        Subject: "EduNAO",
-        Body: document.getElementById('message').value
-    }).then(
-        window.location.href = "index.html",
-    );
+function sendEmail() {
+   Email.send({
+       SecureToken: "2c1fa375-3443-477d-bc0a-0384c5b6aaae",
+       To: document.getElementById('email').value,
+       From: "oleaga717@gmail.com",
+       Subject: "EduNAO",
+       Body: document.getElementById('message').value
+   }).then(
+       window.location.href = "index.html"
+   );
 }
